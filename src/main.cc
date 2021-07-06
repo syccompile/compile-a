177586019dfb7d291e4b88c42d5fcd5f66192fc7
zhj12138 <1550223143@qq.com>
2021-07-06 13:11:09 +0800

    add test module

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,9 @@
 #include "ir_opt.h"
 
 #include <vector>
+#include <fstream>
+#include <iostream>
+#include <iterator>
 
 extern int yylineno;
 
@@ -11,7 +14,12 @@ std::vector<VarDeclStmt *> vardecl;
 /** 函数声明  **/
 std::vector<FunctionDecl *> funcs;
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc < 4) {
+    std::cerr << "usage: compiler <sysy_file> <ir_file> <asm_file>" << std::endl;
+    exit(0);
+  }
+  freopen(argv[1], "r", stdin);
   yylineno = 1;
   yyparse();
 
@@ -24,5 +32,28 @@ int main() {
   }
 
   remove_redunctant_label(ir_list);
+
+  std::ofstream IRFile(argv[2]);
+  auto old_cout_buf = std::cout.rdbuf(IRFile.rdbuf());
   for (const auto &i: ir_list) i->internal_print();
+  std::cout.rdbuf(old_cout_buf);
+  IRFile.close();
+
+  // do some optimization for IR
+
+  std::vector<std::string> asm_vector;
+  for (const auto& ir: ir_list) {
+    auto code = ir->translate_arm();
+    asm_vector.insert(asm_vector.end(),
+                      std::move_iterator(code.begin()),
+                      std::move_iterator(code.end()));
+  }
+
+  // do some optimization for ASM
+
+  std::ofstream ASMFile(argv[3]);
+  for (const auto& code: asm_vector) {
+    ASMFile << code << std::endl;
+  }
+  ASMFile.close();
 }
