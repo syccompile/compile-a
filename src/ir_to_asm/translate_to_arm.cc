47e148b65b73c67bc623bdd29dacd0d5a965e7fd
whsu <2298536003@qq.com>
2021-07-24 16:04:17 +0800

    finished arm generate

--- src/ir_to_asm/translate_to_arm.cc
+++ src/ir_to_asm/translate_to_arm.cc
@@ -53,6 +53,21 @@ get_arm_opcode(IR::Op op) {
     CASE(AND, "and");
     CASE(OR,  "orr");
     CASE(XOR, "eor");
+    CASE(MOV, "mov");
+    CASE(MVN, "mvn");
+    CASE(MOVLE, "movle");
+    CASE(MOVLT, "movlt");
+    CASE(MOVGE, "movge");
+    CASE(MOVGT, "movgt");
+    CASE(MOVEQ, "moveq");
+    CASE(MOVNE, "movne");
+    CASE(JMP, "b");
+    CASE(JLE, "ble");
+    CASE(JLT, "blt");
+    CASE(JGE, "bge");
+    CASE(JGT, "bgt");
+    CASE(JE,  "beq");
+    CASE(JNE, "bne");
     default: return string("");
   }
 
@@ -100,30 +115,49 @@ calculate_stack(IR::List &l, std::unordered_map<int, int> &color_assign) {
   return ret;
 }
 
-// 将VAR型变量移动到内存中
+// 将变量移动到寄存器中
 std::pair<string, std::list<string> >
-move_var_to_reg(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Addr::Ptr ir_addr, std::string tmp_regname) {
+move_to_reg(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Addr::Ptr ir_addr, std::string tmp_regname) {
   
   color addr_color = ir_addr->get_color();
   int   addr_val   = ir_addr->val;
   std::list<string> ret;
   string ret_reg;
 
-  // 如果属于预着色
-  if (addr_color < 4) ret_reg = std::string("r") + to_string(addr_color);
-  // 如果属于寄存器内颜色
-  else if (color_assign.count(addr_color)) ret_reg = std::string("r") + to_string(color_assign[addr_color]);
-  // 如果属于溢出颜色
-  else if (stack.spill.count(addr_color)) {
-    ret_reg = tmp_regname;
-    ret.push_back(string("ldr\t") + tmp_regname + " [sp, #" + to_string(4*(stack.spill_offset()+stack.spill[addr_color]-1)) + "]");
+  if (ir_addr->kind == IR::Addr::Kind::VAR) {
+    // 如果属于预着色
+    if (addr_color < 4) ret_reg = std::string("r") + to_string(addr_color);
+    // 如果属于寄存器内颜色
+    else if (color_assign.count(addr_color)) ret_reg = std::string("r") + to_string(color_assign[addr_color]);
+    // 如果属于溢出颜色
+    else if (stack.spill.count(addr_color)) {
+      ret_reg = tmp_regname;
+      ret.push_back(string("ldr\t") + tmp_regname + " [sp, #" + to_string(4*(stack.spill_offset()+stack.spill[addr_color]-1)) + "]");
+    }
+    // 如果属于局部数组
+    // 那么就将局部数组地址存入count
+    else if (stack.local_arr.count(addr_val)) {
+      ret_reg = tmp_regname;
+      int arr_offset = 4 * (stack.local_arr_offset() + stack.local_arr[addr_val] - 1);
+      ret.push_back(string("add\t") + tmp_regname + " sp, #" + to_string(arr_offset));
+    }
   }
-  // 如果属于局部数组
-  // 那么就将局部数组地址存入count
-  else if (stack.local_arr.count(addr_val)) {
+  else if (ir_addr->kind == IR::Addr::Kind::PARAM) {
+    // 如果属于在寄存器内的参数
+    if (addr_color != color_graph::none_color()) {
+      ret_reg = string("r") + to_string(addr_color);
+    }
+    // 如果属于栈传递的参数
+    else {
+      ret_reg = tmp_regname;
+      int param_offset = 4 * (stack.frame_offset() + addr_val - 4);
+      ret.push_back(string("ldr\t") + tmp_regname + ", [sp, #" + to_string(param_offset));
+    }
+  }
+  // ir_armify保证全局变量一定是变量型的
+  else if (ir_addr->kind == IR::Addr::Kind::NAMED_LABEL) {
     ret_reg = tmp_regname;
-    int arr_offset = 4 * (stack.local_arr_offset() + stack.local_arr[addr_val] - 1);
-    ret.push_back(string("add\t") + tmp_regname + " sp, #" + to_string(arr_offset));
+    ret.push_back(string("ldr\t") + tmp_regname + ", =" + ir_addr->name);
   }
 
   return std::make_pair(ret_reg, ret);
@@ -131,21 +165,40 @@ move_var_to_reg(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, I
 
 // 生成将寄存器内容移动到VAR型变量的语句
 std::pair<string, std::list<string> >
-store_reg_to_var(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Addr::Ptr ir_addr, std::string tmp_regname) {
+store_from_reg(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Addr::Ptr ir_addr, std::string tmp_regname) {
 
   color addr_color = ir_addr->get_color();
   int   addr_val   = ir_addr->val;
   std::list<string> ret;
   string ret_reg;
 
-  // 如果属于预着色
-  if (addr_color < 4) ret_reg = std::string("r") + to_string(addr_color);
-  // 如果属于寄存器内颜色
-  else if (color_assign.count(addr_color)) ret_reg = std::string("r") + to_string(color_assign[addr_color]);
-  // 如果属于溢出颜色
-  else if (stack.spill.count(addr_color)) {
+  if (ir_addr->kind == IR::Addr::Kind::VAR) {
+    // 如果属于预着色
+    if (addr_color < 4) ret_reg = std::string("r") + to_string(addr_color);
+    // 如果属于寄存器内颜色
+    else if (color_assign.count(addr_color)) ret_reg = std::string("r") + to_string(color_assign[addr_color]);
+    // 如果属于溢出颜色
+    else if (stack.spill.count(addr_color)) {
+      ret_reg = tmp_regname;
+      ret.push_back(string("str\t") + tmp_regname + " [sp, #" + to_string(4*(stack.spill_offset()+stack.spill[addr_color]-1)) + "]");
+    }
+  }
+  else if (ir_addr->kind == IR::Addr::Kind::PARAM) {
+    // 如果属于在寄存器内的参数
+    if (addr_color != color_graph::none_color()) {
+      ret_reg = string("r") + to_string(addr_color);
+    }
+    // 如果属于栈传递的参数
+    else {
+      ret_reg = tmp_regname;
+      int param_offset = 4 * (stack.frame_offset() + addr_val - 4);
+      ret.push_back(string("str\t") + tmp_regname + ", [sp, #" + to_string(param_offset));
+    }
+  }
+  // ir_armify保证全局变量一定是变量型的
+  else if (ir_addr->kind == IR::Addr::Kind::NAMED_LABEL) {
     ret_reg = tmp_regname;
-    ret.push_back(string("str\t") + tmp_regname + " [sp, #" + to_string(4*(stack.spill_offset()+stack.spill[addr_color]-1)) + "]");
+    ret.push_back(string("str\t") + tmp_regname + ", =" + ir_addr->name);
   }
 
   return std::make_pair(ret_reg, ret);
@@ -156,9 +209,9 @@ translate(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Ptr
   std::list<std::string> ret;
 
   if (ir->is_al()) {
-    auto [a1_reg, a1_arm] = move_var_to_reg(stack, color_assign, ir->a1, string("r10"));
-    auto [a2_reg, a2_arm] = move_var_to_reg(stack, color_assign, ir->a2, string("fp"));
-    auto [a0_reg, a0_arm] = store_reg_to_var(stack, color_assign, ir->a0, string("fp"));
+    auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+    auto [a2_reg, a2_arm] = move_to_reg(stack, color_assign, ir->a2, string("fp"));
+    auto [a0_reg, a0_arm] = store_from_reg(stack, color_assign, ir->a0, string("fp"));
     // 将a1与a2取入寄存器
     ret.splice(ret.end(), a1_arm);
     ret.splice(ret.end(), a2_arm);
@@ -168,10 +221,86 @@ translate(Stack_Alloc &stack, std::unordered_map<int, int> color_assign, IR::Ptr
     ret.splice(ret.end(), a0_arm);
   }
   else if (ir->is_mov()) {
-    
+    auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+    auto [a0_reg, a0_arm] = store_from_reg(stack, color_assign, ir->a0, string("r10"));
+    // 将a1取入寄存器
+    ret.splice(ret.end(), a1_arm);
+    // 移动
+    ret.push_back(get_arm_opcode(ir->op_) + "\t" + a0_reg + ", " + a1_reg);
+    // 将a0存入相应位置
+    ret.splice(ret.end(), a0_arm);
+  }
+  else if (ir->op_ == IR::Op::CMP) {
+    auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+    auto [a2_reg, a2_arm] = move_to_reg(stack, color_assign, ir->a2, string("fp"));
+    // 将a1与a2取入寄存器
+    ret.splice(ret.end(), a1_arm);
+    ret.splice(ret.end(), a2_arm);
+    ret.push_back(string("cmp") + "\t" + a1_reg + a2_reg);
+  }
+  else if (ir->op_ == IR::Op::LABEL) {
+    ret.push_back(string(".L") + to_string(ir->a0->val));
   }
   else if (ir->is_jmp()) {
+    ret.push_back(get_arm_opcode(ir->op_) + "\t.L" + to_string(ir->a0->val));
+  }
+  else if (ir->op_ == IR::Op::PARAM) {
+    // 前四个参数
+    if (ir->a0->get_color() != color_graph::none_color()) {
+      auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+      // 将a1取入寄存器
+      ret.splice(ret.end(), a1_arm);
+      // 移动
+      ret.push_back(string("mov\t") + "r" + to_string(ir->a0->get_color()) + ", " + a1_reg);
+    }
+    // 后面的参数：压栈
+    else {
+      auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+      int offset = 4*(ir->a0->val-4);
+      ret.push_back(string("str\t") + a1_reg + ", [sp, #" + to_string(offset) + "]");
+    }
+  }
+  else if (ir->op_ == IR::Op::CALL) {
+    ret.push_back(string("bl\t") + ir->a0->name);
+  }
+  else if (ir->op_ == IR::Op::RET) {
+    auto [a0_reg, a0_arm] = move_to_reg(stack, color_assign, ir->a1, string("r0"));
+    // 将a1取入寄存器
+    ret.splice(ret.end(), a0_arm);
+    // 移动
+    ret.push_back(string("mov\tr0") + a0_reg);
 
+    // TODO 返回
+    ret.push_back(string("pop\t"));
+  }
+  // LOAD
+  else if (ir->op_ == IR::Op::LOAD) {
+    auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("fp"));
+    auto [a2_reg, a2_arm] = move_to_reg(stack, color_assign, ir->a2, string("r10"));
+    auto [a0_reg, a0_arm] = store_from_reg(stack, color_assign, ir->a0, string("r10"));
+    // 将a1与a2取入寄存器
+    ret.splice(ret.end(), a1_arm);
+    ret.splice(ret.end(), a2_arm);
+    // 执行变址取数
+    ret.push_back(string("ldr\t") + a0_reg + ", [" + a1_reg + ", " + a2_reg + ",lsl #2]");
+    // 存储a0
+    ret.splice(ret.end(), a0_arm);
+  }
+  // STORE
+  else if (ir->op_ == IR::Op::LOAD) {
+    auto [a0_reg, a0_arm] = move_to_reg(stack, color_assign, ir->a0, string("fp"));
+    auto [a1_reg, a1_arm] = move_to_reg(stack, color_assign, ir->a1, string("r10"));
+    auto [a2_reg, a2_arm] = store_from_reg(stack, color_assign, ir->a2, string("r9"));
+    // 将a0与a1取入寄存器
+    ret.splice(ret.end(), a1_arm);
+    ret.splice(ret.end(), a2_arm);
+    // 将a2取入寄存器，并检查是否需要a9
+    if (a2_reg == string("a9")) ret.push_back(string("str\tr9, [sp, #-4]"));
+    ret.splice(ret.end(), a2_arm);
+    // 执行变址存数
+    ret.push_back(string("str\t") + a2_reg + ", [" + a0_reg + ", " + a1_reg + ",lsl #2]");
+    // 还原a9
+    if (a2_reg == string("a9")) ret.push_back(string("ldr\tr9, [sp, #-4]"));
   }
 }
 
@@ -196,13 +325,20 @@ translate_function(IR::List &l) {
   ret.push_back(name + ":");
 
   // pre-process
-  ret.push_back(string("push\t{lr, r4-r11}"));
-  ret.push_back(string("sub\tsp, sp, #") + to_string(stack.stack_size()));
+  string need_to_save = "r10, fp, lr, ";
+  if (color_assign.size() == 1) need_to_save += "r4";
+  else                          need_to_save += ("{r4-r" + to_string(3+color_assign.size()));
+
+  ret.push_back(string("push\t") + need_to_save);
+  ret.push_back(string("sub\tsp, sp, #") + to_string(stack.frame_offset() - stack.register_save_size));
 
   // actual translate
-  ret.splice(ret.end(), translate(stack));
+  for (auto ir: l)
+    ret.splice(ret.end(), translate(stack, color_assign, ir));
 
   // post-process
-  ret.push_back(string("add\tsp, sp, #") + to_string(stack.stack_size()));
-  ret.push_back(string("pop\t{pc, r4-r11}"));
+  ret.push_back(string("add\tsp, sp, #") + to_string(stack.frame_offset() - stack.register_save_size));
+  ret.push_back(string("pop\t") + need_to_save);
+
+  return ret;
 }
