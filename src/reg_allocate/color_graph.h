5ca5a333613c2043a0421bf65e74a5b965008fd9
Charles-maker-design <55201379+Charles-maker-design@users.noreply.github.com>
2021-08-03 12:31:33 +0800

    Delete unnecessary functions

--- src/reg_allocate/color_graph.h
+++ src/reg_allocate/color_graph.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <set>
 #include <memory>
+#include <utility>
+#include <map>
 
 namespace color_graph{
 
@@ -16,7 +18,7 @@ class color_node {
 public:
   using ptr = std::shared_ptr<color_node>;
   using nodes = std::set<ptr>;
-  color_node() {}
+  color_node(): col(none_color()) {}
   // Check whether node has been colorized
   bool is_colored() { return col != none_color(); }
   // Colorize a node with color c
@@ -24,7 +26,7 @@ public:
   // Get color of a node
   color get_color() { return col; }
   // Get node's neighbors who was colored
-  nodes get_colored_neighbors();
+  nodes get_colored_neighbors(std::map<int, int> num_color_list);
   // Get all the node's neighbors
   virtual nodes get_neighbors() = 0;
   // Check whether a node is neighbored
@@ -52,24 +54,10 @@ public:
 };
 
 
-// 为一系列相邻的节点着色
-// pre-condition: 所有节点相邻的节点都在集合中
-void colorize_nodes(color_node::nodes nodes);
-
-// 尝试使用color_num种颜色为图着色,
-// 可能会不够，所以有些节点会是spill
-// color 表示溢出
-void colorize_num(color_node::nodes nodes, int color_num);
-
 // 使用显式的颜色分配器
 // 对于预着色节点可以
 // 使用颜色分配器为预着色
 // 节点分配颜色后再将该分配器传入
-void colorize_nodes_allocate(color_node::nodes nodes, color_allocate alloc);
+void colorize_nodes_allocate(color_node::nodes nodes, color_allocate alloc, std::map<int, int> &map);
 
-// 使用显式分配器分配器
-// 如果已经为预着色节点
-// 分配颜色 已分配颜色数
-// 也计入alloc_num
-void colorize_num_allocate(color_node::nodes nodes, int color_num, color_allocate alloc);
 }
