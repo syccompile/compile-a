760c6945224bd4669760a0559d0ef345522c6592
Charles-maker-design <55201379+Charles-maker-design@users.noreply.github.com>
2021-08-03 12:32:55 +0800

    Colorize depending on num_color_list

--- src/reg_allocate/color_graph.cc
+++ src/reg_allocate/color_graph.cc
@@ -1,22 +1,28 @@
 #include "color_graph.h"
+#include "flow_graph.h"
 #include <stack>
 #include <map>
+#include <ir_addr.h>
 
 namespace color_graph {
 
 namespace { // helper
 // 为一个节点着色
-void _colorize_node(color_node::ptr p, color_allocate& alloc) {
+void _colorize_node(color_node::ptr p, color_allocate& alloc, map<int, int> &num_color_list) {
+  auto var_p = dynamic_pointer_cast<var>(p);
   if (p->is_colored()) {
     return ;
   }
-  color_node::nodes neighbors = p->get_colored_neighbors();
+  // 得到所有已着色的邻居
+  color_node::nodes neighbors = p->get_colored_neighbors(num_color_list);
   // 如果已着色的邻居数量比已分配颜色的数量更多，
   // 只能分配一个新的颜色
   if (neighbors.size() >= alloc.alloc_num()) {
     p->colorize(alloc.get_new_color());
+    num_color_list.insert(pair<int, int> (var_p->num, p->get_color()));
     return ;
   }
+
   // 如果已着色的邻居数量比已分配的颜色数量少，
   // 必然存在一个已分配过的颜色与邻居的颜色都不冲突
   // 尝试使用一个已分配过的颜色为节点分配
@@ -53,37 +59,21 @@ void color_allocate::reclaim_color() {
   allocated_num--;
 }
 
-void colorize_nodes(color_node::nodes nodes) {
-  color_allocate alloc;
-  colorize_nodes_allocate(nodes, alloc);
-}
 
-void colorize_nodes_allocate(color_node::nodes nodes, color_allocate alloc) {
+void colorize_nodes_allocate(color_node::nodes nodes, color_allocate alloc, map<int, int> &num_color_list) {
+  if(nodes.size()==0) return;
   for (color_node::ptr p : nodes) {
-    _colorize_node(p, alloc);
+    _colorize_node(p, alloc, num_color_list);
   }
 }
 
-void colorize_num_allocate(color_node::nodes nodes, int color_num, color_allocate alloc) {
-  for (color_node::ptr p : nodes) {
-    _colorize_node(p, alloc);
-    // 变量实际溢出
-    if (alloc.alloc_num() > color_num) {
-      alloc.reclaim_color();
-      p->colorize(spill_color());
-    }
-  }
-}
-
-void colorize_num(color_node::nodes nodes, int color_num) {
-  color_allocate alloc;
-  colorize_num_allocate(nodes, color_num, alloc);
-}
-
-color_node::nodes color_node::get_colored_neighbors() {
+// 得到所有已着色邻居
+// num_color_list 记录了num号变量着的色号
+color_node::nodes color_node::get_colored_neighbors(map<int, int> num_color_list) {
   nodes n;
   for (ptr p : get_neighbors()) {
-    if (p->is_colored()) {
+    auto var_p = dynamic_pointer_cast<var>(p);
+    if(num_color_list.find(var_p->num) != num_color_list.end()){
       n.insert(p);
     }
   }
